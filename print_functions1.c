6f4ae9ffa51bc959510c30cfac04c4db97bbdbf2
hassan el faqir <119409354+hassanelfaqir@users.noreply.github.com>
2023-07-25 23:49:01 +0200

    Update print_functions1.c

--- print_functions1.c
+++ print_functions1.c
@@ -6,9 +6,9 @@
  * @types: List a of arguments.
  * @buffer: Buffer array to handle print
  * @flags:  Calculates active flags
- * @width: get width
- * @precision: Precision specification
- * @size: Size specifier
+ * @width: get width.
+ * @precision: Precision specification.
+ * @size: Size specifier.
  * Return: Number of chars printed.
  */
 int print_unsigned(va_list types, char buffer[],
